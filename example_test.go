e4c0e1d1eec7ab37b77231820e21ae3a4575977d
Osamu TONOMORI <osamingo@gmail.com>
2017-02-08 22:37:19 +0900

    Add example jsonrpc test

---
+++ example_test.go
@@ -0,0 +1,68 @@
+package jsonrpc
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"log"
+	"net/http/httptest"
+	"bytes"
+	"io"
+	"os"
+)
+
+type (
+	EchoHandler struct{}
+	EchoParams  struct {
+		Name string `json:"name"`
+	}
+	EchoResult struct {
+		Message string `json:"message"`
+	}
+)
+
+var _ (Handler) = (*EchoHandler)(nil)
+
+func (h *EchoHandler) ServeJSONRPC(c context.Context, params *json.RawMessage) (interface{}, *Error) {
+
+	var p EchoParams
+	if err := Unmarshal(params, &p); err != nil {
+		return nil, err
+	}
+
+	return EchoResult{
+		Message: "Hello, " + p.Name,
+	}, nil
+}
+
+func ExampleJSONRPC() {
+
+	if err := RegisterMethod("Main.Echo", &EchoHandler{}, EchoParams{}, EchoResult{}); err != nil {
+		log.Fatalln(err)
+	}
+
+	http.HandleFunc("/jrpc", HandlerFunc)
+	http.HandleFunc("/jrpc/debug", DebugHandlerFunc)
+
+	srv := httptest.NewServer(http.DefaultServeMux)
+	defer srv.Close()
+
+	resp, err := http.Post(srv.URL+"/jrpc", "application/json", bytes.NewBufferString(`{
+	  "jsonrpc": "2.0",
+      "method": "Main.Echo",
+      "params": {
+        "name": "John Doe"
+      },
+      "id": "243a718a-2ebb-4e32-8cc8-210c39e8a14b"
+    }`))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Output:
+	// {"id":"243a718a-2ebb-4e32-8cc8-210c39e8a14b","jsonrpc":"2.0","result":{"message":"Hello, John Doe"}}
+}
