cc0f67301689f18775d57e2e08fe065a9d9a3f7b
Osamu TONOMORI <osamingo@gmail.com>
2017-03-28 21:22:03 +0900

    Add godoc comment

--- context.go
+++ context.go
@@ -10,10 +10,12 @@ import (
 
 type requestIDKey struct {}
 
+// RequestID takes request id from context.
 func RequestID(c context.Context) *json.RawMessage {
 	return c.Value(requestIDKey{}).(*json.RawMessage)
 }
 
+// WithRequestID adds request id to context.
 func WithRequestID(c context.Context, id *json.RawMessage) context.Context {
 	return context.WithValue(c, requestIDKey{}, id)
 }
