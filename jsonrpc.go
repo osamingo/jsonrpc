42805e20552ebc2b3b1b1f8ed88e23b3a42f4626
s3rj1k <evasive.gyron@gmail.com>
2018-11-19 15:26:43 +0200

    fix gofmt issue
    Signed-off-by: s3rj1k <evasive.gyron@gmail.com>

--- jsonrpc.go
+++ jsonrpc.go
@@ -20,17 +20,17 @@ const (
 type (
 	// A Request represents a JSON-RPC request received by the server.
 	Request struct {
-		Version string               `json:"jsonrpc"`
-		Method  string               `json:"method"`
+		Version string           `json:"jsonrpc"`
+		Method  string           `json:"method"`
 		Params  *json.RawMessage `json:"params"`
 		ID      *json.RawMessage `json:"id"`
 	}
 
 	// A Response represents a JSON-RPC response returned by the server.
 	Response struct {
-		Version string               `json:"jsonrpc"`
-		Result  interface{}          `json:"result,omitempty"`
-		Error   *Error               `json:"error,omitempty"`
+		Version string           `json:"jsonrpc"`
+		Result  interface{}      `json:"result,omitempty"`
+		Error   *Error           `json:"error,omitempty"`
 		ID      *json.RawMessage `json:"id,omitempty"`
 	}
 )
