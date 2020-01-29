76f575ecd765321b5a04ca95bf5d12222fda1233
Алексей Романовский <rav@tochka.com>
2020-01-29 20:04:09 +0500

    Add method metadata to context

--- context.go
+++ context.go
@@ -7,6 +7,7 @@ import (
 )
 
 type requestIDKey struct{}
+type metadataIDKey struct{}
 type methodNameKey struct{}
 
 // RequestID takes request id from context.
@@ -17,6 +18,16 @@ func RequestID(c context.Context) *fastjson.RawMessage {
 // WithRequestID adds request id to context.
 func WithRequestID(c context.Context, id *fastjson.RawMessage) context.Context {
 	return context.WithValue(c, requestIDKey{}, id)
+}
+
+// RequestID takes request id from context.
+func GetMetadata(c context.Context) Metadata {
+	return c.Value(metadataIDKey{}).(Metadata)
+}
+
+// WithRequestID adds request id to context.
+func WithMetadata(c context.Context, md Metadata) context.Context {
+	return context.WithValue(c, metadataIDKey{}, md)
 }
 
 // MethodName takes method name from context.
